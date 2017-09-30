2aa84e1057fc214c56a2ce5485ced7f55cea6b5f
Leonardo Cardoso <leocardosoti@gmail.com>
2017-09-30 14:32:44 -0300

    Classes de conexão com o bd

---
+++ Agenda/ConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class ConnectionFactory
+    {
+        public static SqlConnection GetConnection()
+        {
+            String stringDeConexao = "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=dbagenda;Data Source=DESKTOP-QN093G9";
+
+            SqlConnection conn = new SqlConnection(stringDeConexao);
+
+            return conn;
+        }
+    }
+}
---
+++ Agenda/ContatoDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class ContatoDAO
+    {
+        const string TABLE = "TBCONTATO";
+        SqlCommand command = null;
+        SqlDataReader reader = null;
+
+        public Contato BuscarContatoPorID(int id)
+        {
+            SqlConnection connection = ConnectionFactory.GetConnection();
+            connection.Open();
+
+            string query = "SELECT * FROM " + TABLE + " WHERE ID = @ID ";
+
+            command = new SqlCommand(query, connection);
+
+            SqlParameter param = new SqlParameter();
+
+            param.ParameterName = "@ID";
+            param.Value = id;
+            command.Parameters.Add(param);
+
+            reader = command.ExecuteReader();
+
+            Contato contato = new Contato();
+
+            // Exibindo os registros
+            while (reader.Read())
+            {
+                contato.Nome = reader.GetString(0);
+
+            }
+
+            Console.WriteLine(contato.Nome);
+
+            return contato;
+        }
+    }
+}
